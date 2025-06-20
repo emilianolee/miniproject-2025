d00266d2412042d83d85e51eb7170af30b95ef5b
emilianolee <yechan5172@daum.net>
2025-06-20 12:21:51 +0900

    5일차 미프 커밋

---
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/Models/Process.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfMrpSimulatorApp.Models;
+
+public partial class Process
+{
+    /// <summary>
+    /// 공정처리 순번(자동증가)
+    /// </summary>
+    public int PrcIdx { get; set; }
+
+    public int SchIdx { get; set; }
+
+    /// <summary>
+    /// 공정처리 ID(UK) : yyyyMMdd-NewGuid(36)
+    /// </summary>
+    public string PrcCd { get; set; } = null!;
+
+    public DateOnly? PrcDate { get; set; }
+
+    /// <summary>
+    /// 실제 로드타입
+    /// </summary>
+    public int? PrcLoadTime { get; set; }
+
+    /// <summary>
+    /// 실제 시작시간
+    /// </summary>
+    public TimeOnly? PrcStartTime { get; set; }
+
+    /// <summary>
+    /// 실제 종료시간
+    /// </summary>
+    public TimeOnly? PrcEndTime { get; set; }
+
+    /// <summary>
+    /// 실제 공정 장비 아이디
+    /// 
+    /// </summary>
+    public string? PrcFacilityId { get; set; }
+
+    /// <summary>
+    /// 공정처리 여부(1은 성공 0은  실패)
+    /// </summary>
+    public sbyte? PrcResult { get; set; }
+
+    /// <summary>
+    /// 등록일
+    /// </summary>
+    public DateTime? RegDt { get; set; }
+
+    /// <summary>
+    /// 수정일
+    /// </summary>
+    public DateTime? ModDt { get; set; }
+
+    public virtual Schedule SchIdxNavigation { get; set; } = null!;
+}
---
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/ViewModels/SettingViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMrpSimulatorApp.ViewModels
+{
+    public partial class SettingViewModel : ObservableObject
+    {
+        #region View와 연동할 멤버 변수들
+
+        private string _basicCode;
+        private string _codeName;
+        private string? _codeDesc;
+        private DateTime? _regDt;
+        private DateTime? _modDt; 
+
+        #endregion
+
+        #region View와 연동할 속성
+        public string BasicCode
+        {
+            get => _basicCode;
+            set => SetProperty(ref _basicCode, value);
+        }
+
+        public string CodeName
+        {
+            get => _codeName;
+            set => SetProperty(ref _codeName, value);
+        }
+
+        public string? CodeDesc
+        {
+            get => _codeDesc;
+            set => SetProperty(ref _codeDesc, value);
+        }
+        
+        public DateTime? RegDt {
+            get => _regDt;
+            set => SetProperty(ref _regDt, value); 
+        }
+
+        public DateTime? ModDt { 
+            get => _modDt;
+            set => SetProperty(ref _modDt, value);
+        }
+
+        #endregion
+
+        public SettingViewModel()
+        {
+        }
+    }
+}
